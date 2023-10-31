8c8b48230feda0edb171feb793aa3a7b29e19408
Greshaa1106 <149486950+Greshaa1106@users.noreply.github.com>
2023-10-31 17:17:01 +0530

    Create TransactionManagement.kt

---
+++ TransactionManagement.kt
@@ -0,0 +1,10 @@
+import java.util.Scanner
+import java.util.Random
+
+class TransactionManagement {
+    // Transaction management properties and methods
+    fun processPayment(booking: Booking, amount: Double) {
+        // Implement payment processing logic here (e.g., payment gateway integration)
+        println("Payment processed for booking ${booking.customer.name}. Amount: $amount")
+    }
+}
